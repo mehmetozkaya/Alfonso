2dcea099e19a981170e202f4f0847398991f399c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-07 15:03:55 +0300

    CatalogBrand

---
+++ ApplicationCore/Entities/CatalogBrand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class CatalogBrand : BaseEntity
+    {
+        public string Brand { get; set; }
+    }
+}
