15ee88f48f7b24cf6f4c43b120bb1223515c36e6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-10 20:44:56 +0300

    IAggregateRoot

---
+++ ApplicationCore/Entities/CompareAggregate/Compare.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities.CompareAggregate
+{
+    public class Compare : BaseEntity, IAggregateRoot
+    {
+    }
+}
