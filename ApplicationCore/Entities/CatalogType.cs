003e441db775239aee27df7cf9f6a9e9d653e457
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-10 20:31:23 +0300

    CatalogType

---
+++ ApplicationCore/Entities/CatalogType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public class CatalogType : BaseEntity
+    {
+        public string Type { get; set; }
+    }
+}
