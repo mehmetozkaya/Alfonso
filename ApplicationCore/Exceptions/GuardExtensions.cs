4084449744f098a6b1d60a40a0cae8c6866f50ea
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-11 22:01:07 +0300

    CompareGuards

---
+++ ApplicationCore/Exceptions/GuardExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Exceptions
+{
+    public static class CompareGuards
+    {
+        //public static void NullBasket(this IGuardClause guardClause, int compareId, Compare basket)
+        //{
+        //    if (basket == null)
+        //        throw new CompareNotFoundException(basketId);
+        //}
+    }
+}
