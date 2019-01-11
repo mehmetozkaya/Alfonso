ec742c220a03d8ecbe908c2531e0a45bef8fd869
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-11 22:06:50 +0300

    IRepository

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        T GetSingleBySpec(ISpecification<T> spec);
+        IEnumerable<T> ListAll();
+        IEnumerable<T> List(ISpecification<T> spec);
+        T Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        int Count(ISpecification<T> spec);
+    }
+}
