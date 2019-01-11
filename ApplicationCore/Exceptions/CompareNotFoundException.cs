81e3ae28f642d894e3aec13dccf4a3fb2dfc07b6
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-11 22:02:40 +0300

    CompareNotFoundException

---
+++ ApplicationCore/Exceptions/CompareNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Exceptions
+{
+    public class CompareNotFoundException : Exception
+    {
+        public CompareNotFoundException(int basketId) : base($"No basket found with id {basketId}")
+        {
+        }
+
+        protected CompareNotFoundException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context)
+        {
+        }
+
+        public CompareNotFoundException(string message) : base(message)
+        {
+        }
+
+        public CompareNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
