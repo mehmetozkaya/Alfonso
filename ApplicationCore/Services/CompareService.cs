a02473fad0a1eca070fb7beb4b2df0b5c12a5efe
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 12:49:14 +0300

    CompareService implemented

--- ApplicationCore/Services/CompareService.cs
+++ ApplicationCore/Services/CompareService.cs
@@ -2,11 +2,30 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ApplicationCore.Services
 {
     public class CompareService : ICompareService
     {
+        public Task AddItemToCompare(int compareId, int catalogItemId, decimal price, int quantity)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task DeleteCompareAsync(int compareId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> GetCompareItemCountAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TransferCompareAsync(string anonymousId, string userName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
