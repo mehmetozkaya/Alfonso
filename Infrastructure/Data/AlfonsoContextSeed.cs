7107e9d02f377fc4947324111fe846846f4696ff
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 19:45:43 +0300

    GetPreconfiguredCatalogBrands

--- Infrastructure/Data/AlfonsoContextSeed.cs
+++ Infrastructure/Data/AlfonsoContextSeed.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using ApplicationCore.Entities;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,37 @@ namespace Infrastructure.Data
     {
         public static async Task SeedAsync(AlfonsoContext alfonsoContext, ILoggerFactory loggerFactory, int? retry = 0)
         {
-            
+            int retryForAvailability = retry.Value;
+            try
+            {
+                // TODO: Only run this if using a real database
+                // context.Database.Migrate();
+
+                if (!alfonsoContext.CatalogBrands.Any())
+                {
+                    alfonsoContext.CatalogBrands.AddRange(GetPreconfiguredCatalogBrands());
+                }
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
+
+        private static IEnumerable<CatalogBrand> GetPreconfiguredCatalogBrands()
+        {
+            return new List<CatalogBrand>()
+            {
+                new CatalogBrand() { Brand = "Azure"},
+                new CatalogBrand() { Brand = ".NET" },
+                new CatalogBrand() { Brand = "Visual Studio" },
+                new CatalogBrand() { Brand = "SQL Server" },
+                new CatalogBrand() { Brand = "Other" }
+            };
+        }
+
     }
 }
