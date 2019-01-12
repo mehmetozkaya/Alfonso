e5d9588dc7bac5ffb17d9765757b8a6f22ab2c2c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 19:50:34 +0300

    GetPreconfiguredCatalogTypes

--- Infrastructure/Data/AlfonsoContextSeed.cs
+++ Infrastructure/Data/AlfonsoContextSeed.cs
@@ -1,9 +1,9 @@
 ﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Data
@@ -16,18 +16,24 @@ namespace Infrastructure.Data
             try
             {
                 // TODO: Only run this if using a real database
-                // context.Database.Migrate();
+                alfonsoContext.Database.Migrate();
 
                 if (!alfonsoContext.CatalogBrands.Any())
                 {
                     alfonsoContext.CatalogBrands.AddRange(GetPreconfiguredCatalogBrands());
+                    await alfonsoContext.SaveChangesAsync();
+                }
+
+                if (!alfonsoContext.CatalogTypes.Any())
+                {
+                    alfonsoContext.CatalogBrands.AddRange(GetPreconfiguredCatalogTypes());
+                    await alfonsoContext.SaveChangesAsync();
                 }
 
 
             }
             catch (Exception)
             {
-
                 throw;
             }
         }
@@ -36,11 +42,22 @@ namespace Infrastructure.Data
         {
             return new List<CatalogBrand>()
             {
-                new CatalogBrand() { Brand = "Azure"},
-                new CatalogBrand() { Brand = ".NET" },
-                new CatalogBrand() { Brand = "Visual Studio" },
-                new CatalogBrand() { Brand = "SQL Server" },
+                new CatalogBrand() { Brand = "IPhone"},
+                new CatalogBrand() { Brand = "Samsung"},
+                new CatalogBrand() { Brand = "LG"},
+                new CatalogBrand() { Brand = "Xaomi"},
                 new CatalogBrand() { Brand = "Other" }
+            };
+        }
+
+        private static IEnumerable<CatalogType> GetPreconfiguredCatalogTypes()
+        {
+            return new List<CatalogType>()
+            {
+                new CatalogType() { Type = "Mug"},
+                new CatalogType() { Type = "T-Shirt" },
+                new CatalogType() { Type = "Sheet" },
+                new CatalogType() { Type = "USB Memory Stick" }
             };
         }
 
