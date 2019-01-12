7db9964e840ad114122ab1a2259f146a6273b865
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 19:42:36 +0300

    AlfonsoContextSeed added

---
+++ Infrastructure/Data/AlfonsoContextSeed.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class AlfonsoContextSeed
+    {
+        public static async Task SeedAsync(AlfonsoContext alfonsoContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            
+        }
+    }
+}
