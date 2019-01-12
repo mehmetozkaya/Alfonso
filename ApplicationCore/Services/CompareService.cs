aeabe050e2c77c5e121aa90fbcc8bf313afe853f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 12:48:58 +0300

    CompareService

---
+++ ApplicationCore/Services/CompareService.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Services
+{
+    public class CompareService : ICompareService
+    {
+
+    }
+}
