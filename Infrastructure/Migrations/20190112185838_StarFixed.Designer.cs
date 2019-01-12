4bb303edb84e81db2e08f98271bdb1091d4b8115
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-12 21:59:07 +0300

    star column fixed

--- Infrastructure/Migrations/20190112185838_StarFixed.Designer.cs
+++ Infrastructure/Migrations/20190112185838_StarFixed.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AlfonsoContext))]
-    partial class AlfonsoContextModelSnapshot : ModelSnapshot
+    [Migration("20190112185838_StarFixed")]
+    partial class StarFixed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
