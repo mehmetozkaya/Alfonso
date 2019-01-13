a6f0b3a070e3cf2dc3af4d53a4e827be42313ec4
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 16:58:01 +0300

    UriComposer

--- ApplicationCore/Services/UriComposer.cs
+++ ApplicationCore/Services/UriComposer.cs
@@ -9,5 +9,11 @@ namespace ApplicationCore.Services
     {
         private readonly CatalogSettings _catalogSettings;
 
+        public UriComposer(CatalogSettings catalogSettings) => _catalogSettings = catalogSettings;
+
+        public string ComposePicUri(string uriTemplate)
+        {
+            return uriTemplate.Replace("http://catalogbaseurltobereplaced", _catalogSettings.CatalogBaseUrl);
+        }
     }
 }
