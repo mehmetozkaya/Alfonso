1ede09f408eac07f970925ab3af3b4c69e1a9faa
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 20:48:27 +0300

    SpecificationEvaluator

--- Infrastructure/Repository/SpecificationEvaluator.cs
+++ Infrastructure/Repository/SpecificationEvaluator.cs
@@ -1,9 +1,16 @@
 ﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System.Linq;
 
 namespace Infrastructure.Repository
 {
     public class SpecificationEvaluator<T> where T : BaseEntity
     {
+        public static IQueryable<T> GetQuery(IQueryable<T> inputQuery, ISpecification<T> specification)
+        {
+            var query = inputQuery;
 
+            throw new System.Exception();
+        }
     }
 }
