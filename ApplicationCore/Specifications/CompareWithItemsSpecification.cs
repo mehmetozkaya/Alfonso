563cff8134410621d3daffe799a37bcf57471548
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 17:35:38 +0300

    CompareWithItemsSpecification

---
+++ ApplicationCore/Specifications/CompareWithItemsSpecification.cs
@@ -0,0 +1,16 @@
+﻿using ApplicationCore.Entities.CompareAggregate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class CompareWithItemsSpecification : BaseSpecification<Compare>
+    {
+        public CompareWithItemsSpecification(int compareId)
+           : base(b => b.Id == compareId)
+        {
+            AddInclude(b => b.Items);
+        }
+    }
+}
