48a56e266870e7a30ad9ef2ce95cb16e5f47a315
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 19:29:23 +0300

    EmailSender

---
+++ Infrastructure/Services/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class EmailSender // : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            // TODO: Wire this up to actual email sending logic via SendGrid, local SMTP, etc.
+            return Task.CompletedTask;
+        }
+    }
+}
