e1f1a476af2575a6a401dd0a0409c5172d1bcd4f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 17:01:04 +0300

    BaseSpecification added

---
+++ ApplicationCore/Specifications/BaseSpecification.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public abstract class BaseSpecification<T> : ISpecification<T>
+    {
+        protected BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria { get; }
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+        public List<string> IncludeStrings { get; } = new List<string>();
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
+        public int Take { get; private set; }
+        public int Skip { get; private set; }
+        public bool isPagingEnabled { get; private set; } = false;
+
+
+    }
+}
