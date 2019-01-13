fce4ce7e18dbd4e9deb0c1414f67cd43c9b25cfe
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-13 17:37:51 +0300

    CatalogFilterPaginatedSpecification

---
+++ ApplicationCore/Specifications/CatalogFilterPaginatedSpecification.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Specifications
+{
+    public class CatalogFilterPaginatedSpecification : BaseSpecification<CatalogItem>
+    {
+        public CatalogFilterPaginatedSpecification(int skip, int take, int? brandId, int? typeId)
+            : base(i => (!brandId.HasValue || i.CatalogBrandId == brandId) &&
+                (!typeId.HasValue || i.CatalogTypeId == typeId))
+        {
+            ApplyPaging(skip, take);
+        }
+    }
+}
