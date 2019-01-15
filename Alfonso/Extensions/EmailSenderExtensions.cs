07e092f563d34026ab533d737a7710e04fb5ae62
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-15 16:06:35 +0300

    SendResetPasswordAsync

--- Alfonso/Extensions/EmailSenderExtensions.cs
+++ Alfonso/Extensions/EmailSenderExtensions.cs
@@ -14,5 +14,11 @@ namespace Alfonso.Extensions
             return emailSender.SendEmailAsync(email, "Confirm your email",
                 $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(link)}'>clicking here</a>.");
         }
+
+        public static Task SendResetPasswordAsync(this IEmailSender emailSender, string email, string callbackUrl)
+        {
+            return emailSender.SendEmailAsync(email, "Reset Password",
+                $"Please reset your password by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+        }
     }
 }
