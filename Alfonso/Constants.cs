edcd978eb227e5ff5b079c8ef560ab97afee8187
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-16 15:09:07 +0300

    Constants added

---
+++ Alfonso/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alfonso
+{
+    public static class Constants
+    {
+        public const string BASKET_COOKIENAME = "eShop";
+        public const int ITEMS_PER_PAGE = 10;
+        public const string DEFAULT_USERNAME = "Guest";
+    }
+}
--- Alfonso/Pages/Index.cshtml.cs
+++ Alfonso/Pages/Index.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Alfonso.Interfaces;
 using Alfonso.Models;
 using Alfonso.Services;
+using Alfonso.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,18 +13,26 @@ namespace Alfonso.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ICatalogService _catalogService;
         private readonly ITelefonService _telefonService;
 
-        public IndexModel(ITelefonService telefonService)
+        public IndexModel(ICatalogService catalogService, ITelefonService telefonService)
         {
+            _catalogService = catalogService ?? throw new ArgumentNullException(nameof(catalogService));
             _telefonService = telefonService ?? throw new ArgumentNullException(nameof(telefonService));
         }
 
         public List<Telefon> TelefonList { get; set; }
+        public CatalogIndexViewModel CatalogModel { get; set; } = new CatalogIndexViewModel();
 
         public void OnGet()
         {
             TelefonList = _telefonService.GetTelefons();
+        }
+
+        public async Task OnGet(CatalogIndexViewModel catalogModel, int? pageId)
+        {
+            CatalogModel = await _catalogService.GetCatalogItems(pageId ?? 0, )
         }
     }
 }
