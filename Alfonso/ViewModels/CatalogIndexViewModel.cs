9a0270547a36cb91ee6cd7f46d000de3f0af8e3f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-16 14:12:21 +0300

    CatalogIndexViewModel created

---
+++ Alfonso/ViewModels/CatalogIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alfonso.ViewModels
+{
+    public class CatalogIndexViewModel
+    {
+        public IEnumerable<CatalogItemViewModel> CatalogItems { get; set; }
+        public IEnumerable<SelectListItem> Brands { get; set; }
+        public IEnumerable<SelectListItem> Types { get; set; }
+        public int? BrandFilterApplied { get; set; }
+        public int? TypesFilterApplied { get; set; }
+        public PaginationInfoViewModel PaginationInfo { get; set; }
+    }
+}
