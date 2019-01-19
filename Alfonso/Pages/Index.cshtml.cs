3982d60997cb8e8c9e1b4290bc627ebbd30bcab7
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-19 21:37:36 +0300

    refactoring

--- Alfonso/Pages/Index.cshtml.cs
+++ Alfonso/Pages/Index.cshtml.cs
@@ -1,12 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Alfonso.Interfaces;
-using Alfonso.Models;
-using Alfonso.Services;
 using Alfonso.ViewModels;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Alfonso.Pages
--- Alfonso/Pages/Product.cshtml.cs
+++ Alfonso/Pages/Product.cshtml.cs
@@ -1,12 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Alfonso.Interfaces;
-using Alfonso.Models;
-using Alfonso.Services;
 using Alfonso.ViewModels;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Alfonso.Pages
@@ -24,10 +19,7 @@ namespace Alfonso.Pages
 
         public async Task OnGet(string slug)
         {
-            CatalogItem = await _catalogService.GetCatalogItem(slug);
-
-            //var telefonService = new TelefonService();
-            //Telefon = telefonService.GetTelefons().FirstOrDefault(x => x.Slug == slug);
+            CatalogItem = await _catalogService.GetCatalogItem(slug);            
         }
     }
 }
