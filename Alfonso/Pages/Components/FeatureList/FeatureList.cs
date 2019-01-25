a17ca78da0a50305f3d2fe5e00c004cf2ef4f544
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-01-25 22:35:08 +0300

    fixes

--- Alfonso/Pages/Components/FeatureList/FeatureList.cs
+++ Alfonso/Pages/Components/FeatureList/FeatureList.cs
@@ -17,9 +17,14 @@ namespace Alfonso.Pages.Components.FeatureList
             _featureService = featureService ?? throw new ArgumentNullException(nameof(featureService));
         }
 
+        public int RowCount { get; set; }
+
         public async Task<IViewComponentResult> InvokeAsync(int catalogItemId)
         {            
             var vm = await _featureService.GetFeatures(catalogItemId);
+            int count = vm.Count();
+            RowCount = int.Parse(Math.Ceiling(((decimal)count / 2)).ToString());
+
             return View(vm);
         }
     }
