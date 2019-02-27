ea97effa8de788c775607f04e7b91c833eef79e0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2019-02-27 13:58:51 +0300

    CustomWebApplicationFactory added

---
+++ FunctionalTests/Web/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Alfonso;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionalTests.Web
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+            });
+            
+        }
+    }
+}
